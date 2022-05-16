2639dec4ab20c88205ade7892b40c0f904ce4510
Ruhika <ruhika406@gmail.com>
2022-05-16 08:34:33 +0530

    looping and slicing a slice

--- 04_array_slices_maps/03_slices.go
+++ 04_array_slices_maps/03_slices.go
@@ -7,9 +7,20 @@ import "fmt"
 func main() {
 	slice1 := []float32{2.3, 3.4, 4.5}
 	fmt.Println("length of the slice is ", len(slice1))
-	fmt.Println("Index value of 0 and 2 indices are", slice1[0], slice1[2])
+	fmt.Println("Index value of 0th and 2nd indices are", slice1[0], slice1[2])
 
 	for i, v := range slice1 {
 		fmt.Println(i, v)
 	}
+
+	//looping way2
+	for i := 0; i < 3; i++ {
+
+		fmt.Println(i, slice1[i])
+
+	}
+	//slicing a slice
+	fmt.Println(slice1[:])
+	fmt.Println(slice1[:1])
+
 }
