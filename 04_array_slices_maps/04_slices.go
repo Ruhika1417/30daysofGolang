a98a7ff3ded1500b1c7512a505dc27fad0e340ab
Ruhika <ruhika406@gmail.com>
2022-05-18 23:27:14 +0530

    appending and deleting the elements

--- 04_array_slices_maps/04_slices.go
+++ 04_array_slices_maps/04_slices.go
@@ -14,4 +14,15 @@ func main() {
 	slice3 := append(slice1, slice2...) //dots afterwards means take all the values of slice2 and put them right here.
 	fmt.Println("The new slice is :", slice3)
 
+	// **special case appending string to a byte**
+	//bs1 := []byte("hello")
+	//bs2 := append(bs1, "hello"...)
+	//fmt.Println(bs2)
+	slice45 := append([]byte("hello"), "world"...)
+	fmt.Println(slice45)
+
+	//deleting elements of a slice using append
+	slice4 := append(slice1[:1], slice2[:]...)
+	fmt.Println(slice4)
+
 }
