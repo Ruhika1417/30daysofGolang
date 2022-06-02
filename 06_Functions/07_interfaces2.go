6e155afe84546811a573c6dc99104ad3d78038c4
Ruhika <ruhika406@gmail.com>
2022-06-02 20:39:52 +0530

    interfaces 2

--- 06_Functions/07_interfaces2.go
+++ 06_Functions/07_interfaces2.go
@@ -17,8 +17,25 @@ type info struct {
 	ltk bool
 }
 
-func (i info) speak() string {
-	fmt.Println("I am ", i.fname, i.age)
+func (p person) speak() {
+	fmt.Println("I am", p.fname, p.lname)
+}
+
+func (i info) speak() {
+	fmt.Println("I am", i.fname, i.lname, i.ltk)
+}
+
+func bar(h human) {
+	switch h.(type) {
+	case person:
+		fmt.Println("Person", h.(person).fname)
+	case info:
+		fmt.Println("Info", h.(info).fname)
+
+	}
+
+	fmt.Println("I'm passed in Bar", h)
+
 }
 
 func main() {
@@ -31,12 +48,17 @@ func main() {
 		ltk: true,
 	}
 
-	i2 := person{
-		person: person{"Monica", "Geller"},
+	p1 := person{
+		"Ross",
+		"Geller",
 	}
 
-	p1 :=
-		i1.speak()
-	fmt.Println(p1)
+	fmt.Println(i1)
+	i1.speak()
+	p1.speak()
 
+	bar(p1)
+	bar(i1)
+	//bar can take in both person and info. coz both are also of type  human interface
+	//polymorphism
 }
