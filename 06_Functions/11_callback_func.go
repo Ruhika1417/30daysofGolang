92a053c55e499de2dd51307c6feeb55dcf4c60f5
Ruhika <ruhika406@gmail.com>
2022-06-02 14:46:35 +0530

    callback functions

--- 06_Functions/11_callback_func.go
+++ 06_Functions/11_callback_func.go
@@ -2,16 +2,32 @@ package main
 
 import "fmt"
 
-func bar(a string) string {
-	return a
-}
-
-func foo(f func(a string) string) {
-	fmt.Println("string")
-
-}
-
 func main() {
-	f := foo(bar("Ruhika"))
-	fmt.Println(f)
+	xii := []int{1, 2, 3, 4, 5, 6}
+	//a := sum(xii...)
+	//fmt.Println("all numbers", a)
+
+	a2 := even(sum, xii...)
+	fmt.Println(a2)
+}
+
+func sum(x ...int) int {
+	total := 0
+	for _, v := range x {
+		total += v
+	}
+	return total
+
+}
+
+func even(f func(x ...int) int, vi ...int) int {
+
+	var y []int
+	for _, v := range vi {
+		if v%2 == 0 {
+			y = append(y, v)
+		}
+	}
+
+	return f(y...)
 }
