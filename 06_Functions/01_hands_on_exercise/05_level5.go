918769454016524e18a16f2dfc77299194c6c3f3
Ruhika <ruhika406@gmail.com>
2022-06-04 20:56:10 +0530

    hands on exercise

---
+++ 06_Functions/01_hands_on_exercise/05_level5.go
@@ -0,0 +1,63 @@
+/* Hands-on exercise #5
+● create a type SQUARE
+● create a type CIRCLE
+● attach a method to each that calculates AREA and returns it
+○ circle area= π r 2
+○ square area = L * W
+● create a type SHAPE that defines an interface as anything that has the AREA method
+● create a func INFO which takes type shape and then prints the area
+Todd McLeod - Learn To Code Go on Udemy - Part 1 - Page 54
+● create a value of type square
+● create a value of type circle
+● use func info to print the area of square
+● use func info to print the area of circle */
+
+package main
+
+import "fmt"
+
+type square struct {
+	length, width float32
+}
+
+type circle struct {
+	radius int
+}
+
+func (s square) area() int {
+
+	return int(s.length) * int(s.width)
+}
+
+func (c circle) area() int {
+	pie := 3.14
+	return 2 * int(pie) * c.radius
+}
+
+type shape interface {
+	area() int
+	// square()
+	// circle()
+}
+
+func info(s shape) {
+
+	fmt.Println(s.area())
+
+}
+
+func main() {
+
+	s1 := square{
+		length: 32.3,
+		width:  23.4,
+	}
+
+	c1 := circle{
+		radius: 69,
+	}
+
+	info(s1)
+	info(c1)
+
+}
