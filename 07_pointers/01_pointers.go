5df891e7e5243a61fa14c15fc7665ef6006b00c4
Ruhika <ruhika406@gmail.com>
2022-06-11 18:54:45 +0530

    pointers

--- 07_pointers/01_pointers.go
+++ 07_pointers/01_pointers.go
@@ -7,6 +7,13 @@ func main() {
 	fmt.Printf("type is %T and value is  %v\n", a, a)
 	fmt.Printf("type is %T and value is %v\n", &a, &a)
 
+	// var b int = &a wrong coz it's 2 different types
 	var b *int = &a
-	fmt.Println(b, *b)
+	fmt.Println(b) //b stores an address, with *b we're dereferencing that address
+	fmt.Println(*b)
+	fmt.Println(*&a)
+
+	*b = 1111 //b stores an address, dereferencing the address and storing the value will change value of a
+	fmt.Println("22 is changed to new value: ", a)
+
 }
