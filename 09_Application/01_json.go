7cebfa075d28987a540d5014dc819cc4b6e98736
Ruhika <ruhika406@gmail.com>
2022-06-17 23:58:16 +0530

    marshal json

--- 09_Application/01_json.go
+++ 09_Application/01_json.go
@@ -1,1 +1,35 @@
+package main
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Person struct {
+	First string
+	Last  string
+	Age   int
+}
+
+func main() {
+	p1 := Person{
+		First: "Rachel",
+		Last:  "Green",
+		Age:   27,
+	}
+	p2 := Person{
+		First: "Ross",
+		Last:  "Geller",
+		Age:   27,
+	}
+	fmt.Println(p1)
+
+	people := []Person{p1, p2}
+	fmt.Println(people)
+
+	byteb, err := json.Marshal(people)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(byteb))
+}
